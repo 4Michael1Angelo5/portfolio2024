8f3feb0dc652d53749cb11a29870cdbb91d3e3bb
Chris Chun <4michael1angelo5@gmail.com>
2025-12-11 14:34:56 -0800

    updated projects and portfolio page opening paragraph

--- src/App.js
+++ src/App.js
@@ -102,14 +102,7 @@ const App = () => {
                           <ContentComponent
                                   content ={PORTFOLIO}
                                   pageTitle={"Portfolio"}
-                                  headLine={
-                                      "When I’m not working, I am constantly involved in side projects \
-                                       and have a wide array of interests. From recreational math \
-                                       projects to encryption, these side projects are what keeps \
-                                       me passionate about coding. They lead me down diverse fields of \
-                                       study, challenge me to acquire new skills, and keep me up to date \
-                                       with emerging technologies. Check out some of my latest work. "
-                                  }
+                                  headLine={"Check out my latest work"}
                           />
                       }
               />
@@ -119,7 +112,14 @@ const App = () => {
                       {
                           <ContentComponent
                               content ={PROJECTS}
-                              headLine={"Check out my latest work"}
+                              headLine = {
+                                  "When I’m not working, I am constantly involved in side projects \
+                                         and have a wide array of interests. From recreational math \
+                                         projects to encryption, these side projects are what keeps \
+                                         me passionate about coding. They lead me down diverse fields of \
+                                         study, challenge me to acquire new skills, and keep me up to date \
+                                         with emerging technologies. Check out some of my latest work. "
+                              }
                               pageTitle={"projects"}
                           />
                       }
